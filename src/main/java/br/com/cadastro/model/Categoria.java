78f772f56217726424e8516c4c0116847c982b6a
Cauã H. Oliveira <151974421+CauaCHO@users.noreply.github.com>
2026-05-26 20:09:53 -0300

    feat: criar model Categoria

---
+++ src/main/java/br/com/cadastro/model/Categoria.java
@@ -0,0 +1,40 @@
+package br.com.cadastro.model;
+
+public class Categoria {
+    private Integer id;
+    private String nomeCategoria;
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nomeCategoria, String descricao) {
+        this.id = id;
+        this.nomeCategoria = nomeCategoria;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
