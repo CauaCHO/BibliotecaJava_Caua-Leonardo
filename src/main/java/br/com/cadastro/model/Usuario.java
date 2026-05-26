bf18f8989c98e4ad6a9a83f4d407329912cf3844
Cauã H. Oliveira <151974421+CauaCHO@users.noreply.github.com>
2026-05-26 20:16:36 -0300

    refactor: relacionar Usuario com Estado

--- src/main/java/br/com/cadastro/model/Usuario.java
+++ src/main/java/br/com/cadastro/model/Usuario.java
@@ -5,15 +5,22 @@ public class Usuario {
     private String nomeUsuario;
     private String cpfCnpj;
     private String email;
+    private Integer estadoId;
+    private String nomeEstado;
+    private String siglaEstado;
 
     public Usuario() {
     }
 
-    public Usuario(Integer id, String nomeUsuario, String cpfCnpj, String email) {
+    public Usuario(Integer id, String nomeUsuario, String cpfCnpj, String email,
+                   Integer estadoId, String nomeEstado, String siglaEstado) {
         this.id = id;
         this.nomeUsuario = nomeUsuario;
         this.cpfCnpj = cpfCnpj;
         this.email = email;
+        this.estadoId = estadoId;
+        this.nomeEstado = nomeEstado;
+        this.siglaEstado = siglaEstado;
     }
 
     public Integer getId() {
@@ -46,5 +53,29 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Integer getEstadoId() {
+        return estadoId;
+    }
+
+    public void setEstadoId(Integer estadoId) {
+        this.estadoId = estadoId;
+    }
+
+    public String getNomeEstado() {
+        return nomeEstado;
+    }
+
+    public void setNomeEstado(String nomeEstado) {
+        this.nomeEstado = nomeEstado;
+    }
+
+    public String getSiglaEstado() {
+        return siglaEstado;
+    }
+
+    public void setSiglaEstado(String siglaEstado) {
+        this.siglaEstado = siglaEstado;
     }
 }
