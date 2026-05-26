668ca153cd7a18904a8923d3482f5ddaf0b2329b
Cauã H. Oliveira <151974421+CauaCHO@users.noreply.github.com>
2026-05-25 22:18:40 -0300

    fix: migrar LivroServlet para javax.servlet

--- src/main/java/br/com/cadastro/servlet/LivroServlet.java
+++ src/main/java/br/com/cadastro/servlet/LivroServlet.java
@@ -2,11 +2,11 @@ package br.com.cadastro.servlet;
 
 import br.com.cadastro.dao.LivroDAO;
 import br.com.cadastro.model.Livro;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.math.BigDecimal;
